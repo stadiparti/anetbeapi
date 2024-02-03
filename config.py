b9a17853d7aa8048f3dc280756a57ede20c5a005
srini <stadiparti@gmail.com>
2024-02-03 12:01:02 -0800

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ API_LOGIN_ID = "5TD4cGmcL84Q"
 TRANSACTION_KEY = "6C6rE54KUf6p42V4"
 REST_API_URL = "https://apitest.authorize.net/rest/invoicing/v2/invoices"
 API_AUTHORIZATION_HEADER = "Basic bmV3LXZpdGFsLWZuYm86MTIzYWJj"  # Replace with your actual encoded API credentials
-REDIS_URL = "redis://red-cmtaa30l6cac73asujcg:6379"
+REDIS_URL = "rediss://red-cmtaa30l6cac73asujcg:wJrA7E9LXXZ1afUBaLlqFQjY6nLdgIw0@oregon-redis.render.com:6379"
 
